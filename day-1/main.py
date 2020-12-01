5b31900d3038778246d6f4cc38c21933205b9498
rreest <raimondreest@hotmail.com>
2020-12-01 11:54:51 +0200

    Day 1

--- day-1/main.py
+++ day-1/main.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-# Template for solution code 
+# Day 1 challenge 
 
-data = open('input').read().strip('\n')
-print(data)
+import itertools
+
+data = [int(a.strip('\n')) for a in open('input').readlines()]
+tdata = [1721, 979, 366, 299, 675, 1456]
+
+# Part 1 - Answer: 1020099
+print([a * b for a, b in itertools.combinations(data, 2) if a + b == 2020])
+
+# Part 2 - Answer: 49214880
+print([a * b * c for a, b, c in itertools.combinations(data, 3) if a + b + c == 2020])
 
